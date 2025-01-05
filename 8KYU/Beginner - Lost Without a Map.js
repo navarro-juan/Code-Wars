4bf19f4eec8760241831083f68de680fe247e57d
Juan <navarrojgr@gmail.com>
2025-01-04 21:36:36 -0600

    added solution

---
+++ 8KYU/Beginner - Lost Without a Map.js
@@ -0,0 +1,6 @@
+// Instructions 
+// Given an array of integers, return a new array with each value doubled.
+
+// My Solution
+// map through array and double the value
+const maps = x => x.map(num => num * 2);
