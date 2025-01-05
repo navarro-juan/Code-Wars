397ae4222f3a30ea47a3501500ad938aa75153e8
Juan <navarrojgr@gmail.com>
2025-01-05 09:45:56 -0600

    added solution

---
+++ 8KYU/Reversed Strings.js
@@ -0,0 +1,10 @@
+// Instructions 
+// Complete the solution so that it reverses the string passed into it.
+
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+
+
+// My solution
+
+const solution = str => str.split("").reverse().join('')
