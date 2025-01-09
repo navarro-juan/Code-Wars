b6cbed3fb2b71ec58bc7f18fb12eab96f947d647
Juan <navarrojgr@gmail.com>
2025-01-09 12:45:26 -0600

    added solution

---
+++ 8KYU/you-cant-code-under-pressure.js
@@ -0,0 +1,11 @@
+/*
+ ======= Problem Name: 
+You Can't Code Under Pressure #1
+
+ ======= Problem description:
+  Code as fast as you can! You need to double the integer and return it.
+
+ ======= Solution:
+*/
+const doubleInteger = i => i*2
+
