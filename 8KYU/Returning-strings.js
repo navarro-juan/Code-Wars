8b73b7368dff1f2f57e4bcdd61953e5cb8b544ea
Juan <navarrojgr@gmail.com>
2025-01-09 12:25:44 -0600

    added solution

---
+++ 8KYU/Returning-strings.js
@@ -0,0 +1,16 @@
+/*
+ ======= Problem Name: 
+Returning Strings
+ ======= Problem description:
+  Make a function that will return a greeting statement that uses an input; 
+  your program should return, "Hello, <name> how are you doing today?".
+
+[Make sure you type the exact thing I wrote or the program may not execute properly]
+
+ ======= Solution:
+    // [Your solution code goes here]
+*/
+
+const greet = name => {
+    return `Hello, ${name} how are you doing today?`
+  }
