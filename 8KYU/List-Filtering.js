2f5c5b4b2718017adc3c8979f77b5d151116ae57
Juan <navarrojgr@gmail.com>
2025-01-28 16:06:55 -0600

    added solution

---
+++ 8KYU/List-Filtering.js
@@ -0,0 +1,14 @@
+/*
+ ======= Problem Name: 
+List Filtering
+
+ ======= Problem description:
+In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+
+ ======= Solution:
+*/
+
+function filter_list(l) {
+    // Return a new array with the strings filtered out
+    return l.filter((l)=> l >= 0 && typeof l === "number")
+  }
