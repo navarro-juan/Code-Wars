19f5f3d33760616a697bc276faa99231e307acb2
Juan <navarrojgr@gmail.com>
2025-01-28 16:04:14 -0600

    added solution

---
+++ 8KYU/Sum-The-Strings.js
@@ -0,0 +1,14 @@
+/*
+ ======= Problem Name: 
+Sum The Strings
+
+ ======= Problem description:
+Create a function that takes 2 nonnegative integers in form of a string as an input, and outputs the sum (also as a string):
+
+ ======= Solution:
+*/
+function sumStr(a,b) {
+    const A = Number(a)
+    const B = Number(b)
+    return (A + B).toString()
+    }
