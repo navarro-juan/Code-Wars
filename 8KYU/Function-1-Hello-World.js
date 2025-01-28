511ebd6c6f32e9432bb4273f5f3d32d4f09f4020
Juan <navarrojgr@gmail.com>
2025-01-28 16:02:46 -0600

    added solution

---
+++ 8KYU/Function-1-Hello-World.js
@@ -0,0 +1,11 @@
+/*
+ ======= Problem Name: 
+Function 1 - hello world
+
+ ======= Problem description:
+Make a simple function called greet that returns the most-famous "hello world!".
+
+ ======= Solution:
+*/
+const greet = () => "hello world!"
+
